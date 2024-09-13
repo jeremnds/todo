c7528c41b4836b5a2dfd71c4c752dbe4b76575a6
jeremnds <jrmymnds@gmail.com>
2024-09-13 14:57:14 +0200

    fix: resolve bug on cookie for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,7 +43,7 @@ app.use(
       maxAge: 60000 * 60 * 2,
       httpOnly: true,
       secure: nodeEnv === "production",
-      sameSite: "lax",
+      sameSite: "none",
     },
     store: MongoStore.create({ mongoUrl: mongoUri }),
   })
